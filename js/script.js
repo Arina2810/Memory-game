1dd33f461bcee8f17fc059ebb99bd92054379ac3
Arina2810 <abuivan@inbox.ru>
2026-02-11 11:14:45 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,53 @@
-const cards = document.querySelectorAll('.card');/*выбираем элемент с классом card  на странице*/
-for (let card of cards){
-card.addEventListener("click", ()=>{
-    card.classList.add("card-show");
-});
+const cards = document.querySelectorAll('.card'); //выбираем все элементы с классом card на странице 
+let openCards = [];
+let card1
+let count = 0;
+const text = document.querySelector(".count")
+text.textContent = "Количество ходов: " + count
+for (let card of cards) {
+    card.addEventListener("click", () => {
+        let pathImg = card.querySelector('img').src;
+        if (openCards.length == 0) {
+            //если масиив пустой
+            card1 = card;
+            openCards.push(pathImg);
+            card.classList.add("card-show");
+        }
 
+        else if (openCards.length == 1) {
+            count++;
+            text.textContent = "Количество ходов: " + count
+            openCards.push(pathImg);
+            card.classList.add("card-show");
+
+
+            if (openCards[0] == openCards[1]) {
+                setTimeout(function () {
+                    card.classList.add("card-finish");
+                    card1.classList.add("card-finish");
+                }, 2000);
+            } else {
+                setTimeout(function () {
+                    card.classList.remove("card-show");
+                    card1.classList.remove("card-show");
+                }, 3000);
+            }
+            openCards = []
+
+        }
+        console.log(openCards);
+    });
 }
- 
+
+
+
+// const h1 = document.querySelector('h1');
+// setTimeout(function(){
+//     h1.style.color = "red";
+// }, 5000)
+
+// let i = 0;
+// setInterval(function(){
+//     i++;
+//     h1.innerHTML= i;
+// }, 1000);
